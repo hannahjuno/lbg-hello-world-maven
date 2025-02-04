1ae2d4a6b3dc7023c5fec948fbd1f6a3b9bdd927
ewie <ewie.com>
2025-02-04 14:17:55 +0000

    changed names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("wallflower"));
+        System.out.println(sayHelloToSomeone("hannah"));
+        System.out.println(sayHelloToSomeone("juno"));
+        System.out.println(sayHelloToSomeone("gavi"));
         System.out.println(sayGoodbye());
     }
 
